172b04cda15626208abf19a404538a41d4fdea70
Shibily-kms <kmsshibily@gmail.com>
2023-05-15 15:50:20 +0530

    change duration to minuts

--- src/components/admin/staff-work/Staff_work_table.jsx
+++ src/components/admin/staff-work/Staff_work_table.jsx
@@ -127,7 +127,7 @@ function Staff_work_table() {
                                     <td>Regular works :</td>
                                     <td>Time start</td>
                                     <td>Time end</td>
-                                    <td>Duration (sec)</td>
+                                    <td>Duration (min)</td>
                                   </tr>
 
                                   <tr style={{ color: 'gray' }}>
