40d80c5592a1a9e921907307ae2bcae81593aeec
Shibily-kms <kmsshibily@gmail.com>
2023-07-06 18:55:59 +0530

    test 1

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -45,7 +45,7 @@ function User() {
           designation: {
             ...user.designation,
             allow_sales: response.data.designation?.allow_sales || false,
-            auto_punch_out: nowTime > '13:29' ? user?.designation?.auto_punch_out || response.data.designation?.auto_punch_out :
+            auto_punch_out: nowTime > '21:00' ? user?.designation?.auto_punch_out || response.data.designation?.auto_punch_out :
               response.data.designation?.auto_punch_out || '17:30',
           }
         }))
