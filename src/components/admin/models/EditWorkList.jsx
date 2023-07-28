a96378c125a68ac8f406abe8c5a5a0bc7e153013
Shibily-kms <kmsshibily@gmail.com>
2023-07-28 10:52:16 +0530

    edit work list removed 28-07-23

--- src/components/admin/models/EditWorkList.jsx
+++ src/components/admin/models/EditWorkList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './edit-work-list.scss';
-import EditTextInput from '../edit-work-input/EditTextInput';
+import WorkText from '../work-text/WorkText';
 import AddTextInput from '../add-work-input/AddTextInput';
 import SpinWithMessage from '../../common/spinners/SpinWithMessage';
 import { adminAxios } from '../../../config/axios'
@@ -8,7 +8,6 @@ import { IoTrashBin } from 'react-icons/io5'
 
 function EditWorkList({ setModel, designationId }) {
     const [works, setWorks] = useState([])
-    const [nowEdit, setNowEdit] = useState('')
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
@@ -32,7 +31,7 @@ function EditWorkList({ setModel, designationId }) {
                 </> : <>
                     {works?.[0] ? <ol>
                         {works.map((obj, index) => <li key={index}>
-                            <EditTextInput work={obj} nowEdit={nowEdit} setNowEdit={setNowEdit} setWorks={setWorks} />
+                            <WorkText work={obj}  setWorks={setWorks} />
                         </li>)}
                     </ol> :
                         <div className='no-work'>
