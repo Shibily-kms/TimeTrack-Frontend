2d3ea6603856ac9596460276f6fde3aca582f0de
Shibily-kms <kmsshibily@gmail.com>
2025-03-23 13:24:38 +0530

    cookie rechanged

--- src/config/axios.js
+++ src/config/axios.js
@@ -19,12 +19,11 @@ const baseSetup = {
 //*  Response and Request Config Functions
 const doSignOut = async () => {
     const cookieOptions = {
-        secure: true, // Ensure secure transmission (Use HTTPS)
-        sameSite: 'none', // Allows cross-site cookies (important for subdomains)
-        path: '/', // Makes the cookie accessible to all routes
-        domain: '.alliancewatersolutions.com', // Allows sharing between subdomains
-        httpOnly: true, // Prevents JavaScript access for security (optional)
-        expires: new Date(Date.now() + 40 * 24 * 60 * 60 * 1000) // Set expiration correctly
+        secure: true,
+        sameSite: 'None',
+        domain: '.alliancewatersolutions.com',
+        path: '/',
+        expires: new Date(new Date().setMonth(new Date().getMonth() + 6))
     };
 
     Cookies.remove('_rfs_tkn')
--- src/redux/features/user/systemSlice.js
+++ src/redux/features/user/systemSlice.js
@@ -25,12 +25,10 @@ export const systemSlice = createSlice({
             state.theme = action?.payload
 
             const cookieOptions = {
-                secure: true, // Ensure secure transmission (Use HTTPS)
-                sameSite: 'none', // Allows cross-site cookies (important for subdomains)
-                path: '/', // Makes the cookie accessible to all routes
-                domain: '.alliancewatersolutions.com', // Allows sharing between subdomains
-                httpOnly: true, // Prevents JavaScript access for security (optional)
-                expires: new Date(Date.now() + 180 * 24 * 60 * 60 * 1000) // Set expiration correctly
+                secure: true,
+                sameSite: 'None',
+                domain: '.alliancewatersolutions.com',
+                path: '/'
             };
             Cookies.set('color_mode', action?.payload, cookieOptions)
         },
