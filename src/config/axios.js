12ffd8c075041ee1ca24bf8fbdec863c8e6dfe99
Shibily-kms <kmsshibily@gmail.com>
2026-06-17 15:09:53 +0530

    url changes applied

--- src/config/axios.js
+++ src/config/axios.js
@@ -14,8 +14,8 @@ const baseSetup = {
     //? v2.1
     ttSv2Axios: axios.create({ baseURL: `${baseUrl}:8000/s/v2/`, headers: apiHeaders }),
     ttCv2Axios: axios.create({ baseURL: `${baseUrl}:8000/c/v2/`, headers: apiHeaders }),
-    cnPv2Axios: axios.create({ baseURL: `https://controlnex.api.alliancedev.in/p/v2/`, headers: apiHeaders, timeout: 60000, }),
-    slUv1Axios: axios.create({ baseURL: `https://sales.api.alliancedev.in/u/v1/`, headers: apiHeaders })
+    cnPv2Axios: axios.create({ baseURL: `https://api.controlnex.alliancewatersolutions.com/p/v2/`, headers: apiHeaders, timeout: 60000, }),
+    slUv1Axios: axios.create({ baseURL: `https://api.salesV1.alliancewatersolutions.com/u/v1/`, headers: apiHeaders })
 }
 
 //*  Response and Request Config Functions
--- src/pages/user/home/Home.jsx
+++ src/pages/user/home/Home.jsx
@@ -118,15 +118,15 @@ function Home({ setPageHead }) {
 
           {(user?.allowed_origins?.includes('WH_Service')) && <div className="app-item">
             <div className="icon-div" style={{ backgroundColor: "#5f5f5f" }}
-              onClick={() => window.location.href = `https://wholehouse.alliancedev.in?id=${user?.acc_id}`}>
+              onClick={() => window.location.href = `https://wholehouse.alliancewatersolutions.com?id=${user?.acc_id}`}>
               <TbCarouselHorizontal />
             </div>
             <p>VF Card Portal</p>
           </div>}
 
-          {(user?.allowed_origins?.includes('WH_Service')) && <div className="app-item">
+          {(user?.allowed_origins?.includes('WH_Admin')) && <div className="app-item">
             <div className="icon-div" style={{ backgroundColor: "#7d8f1a" }}
-              onClick={() => window.location.href = `https://wholehouse.alliancedev.in/admin?id=${user?.acc_id}`}>
+              onClick={() => window.location.href = `https://wholehouse.alliancewatersolutions.com/admin?id=${user?.acc_id}`}>
               <TbCheck />
             </div>
             <p>VF Admin</p>
