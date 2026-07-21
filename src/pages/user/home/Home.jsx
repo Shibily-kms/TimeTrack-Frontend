e3288514293bf8ba62e9a07d9e5b9c38267b9dca
Shibily-kms <kmsshibily@gmail.com>
2026-07-21 14:54:32 +0530

    fix: update vessel and whole house URLs for user access

--- src/pages/user/home/Home.jsx
+++ src/pages/user/home/Home.jsx
@@ -117,7 +117,7 @@ function Home({ setPageHead }) {
           </div>}
 
           {(user?.allowed_origins?.includes('vessel_t_worker')) && <div className="app-item">
-            <div className="icon-div" onClick={() => window.location.href = `https://vessel.alliancedev.in/tech`}>
+            <div className="icon-div" onClick={() => window.location.href = `https://vessel.alliancewatersolutions.com/tech`}>
               <img alt='app-icon' src={Vessel} draggable={false} />
               <span >  <Alliance width={'40px'} hight={'40px'} /> </span>
             </div>
@@ -125,7 +125,7 @@ function Home({ setPageHead }) {
           </div>}
 
           {user?.allowed_origins?.some((access) => access?.slice(0, 8) === 'vessel_c') && <div className="app-item">
-            <div className="icon-div" onClick={() => window.location.href = `https://vessel.alliancedev.in/controller`}>
+            <div className="icon-div" onClick={() => window.location.href = `https://vessel.alliancewatersolutions.com/controller`}>
               <img alt='app-icon' src={VesselAdmin} draggable={false} />
               <span >  <Alliance width={'40px'} hight={'40px'} /> </span>
             </div>
@@ -134,7 +134,7 @@ function Home({ setPageHead }) {
 
           {(user?.allowed_origins?.includes('WH_Service')) && <div className="app-item">
             <div className="icon-div" style={{ backgroundColor: "#5f5f5f" }}
-              onClick={() => window.location.href = `http://localhost:3001?id=${user?.acc_id}`}>
+              onClick={() => window.location.href = `https://wholehouse.alliancewatersolutions.com?id=${user?.acc_id}`}>
               <TbCarouselHorizontal />
             </div>
             <p>VF Card Portal</p>
@@ -142,7 +142,7 @@ function Home({ setPageHead }) {
 
           {(user?.allowed_origins?.includes('WH_Admin')) && <div className="app-item">
             <div className="icon-div" style={{ backgroundColor: "#7d8f1a" }}
-              onClick={() => window.location.href = `http://localhost:3001/admin?id=${user?.acc_id}`}>
+              onClick={() => window.location.href = `https://wholehouse.alliancewatersolutions.com/admin?id=${user?.acc_id}`}>
               <TbCheck />
             </div>
             <p>VF Admin</p>
